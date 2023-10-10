330ddc2260146174d1bd6cbe62ee758989929977
Ahmed-D007A <ahmed.mamdouh8840@gmail.com>
2023-10-10 21:16:05 +0300

    will it work

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
+""" Exporting csv files"""
+import json
 import requests
-"""
-number_of_subscribers module.
-"""
+import sys
 
 
 def number_of_subscribers(subreddit):
-    """ GET subscriber count of a given subreddit """
-    url = "http://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'user-agent': 'gg'}
-    r = requests.get(url, headers=headers)
-    if (r.status_code is 302 or r.status_code is 404):
-        return 0
-    r = r.json()
-    if ('error' in r):
-        return 0
-    elif ('subscribers' in r['data']):
-        return r['data']['subscribers']
+    """Read reddit API and return number subscribers """
+    username = 'ledbag123'
+    password = 'Reddit72'
+    user_pass_dict = {'user': username, 'passwd': password, 'api_type': 'json'}
+    headers = {'user-agent': '/u/ledbag123 API Python for Holberton School'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    client = requests.session()
+    client.headers = headers
+    r = client.get(url, allow_redirects=False)
+    if r.status_code == 200:
+        return (r.json()["data"]["subscribers"])
     else:
-        return 0
+        return(0)
