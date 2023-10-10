ada48c28da4d001506fde2fff77bba9ba925e631
Ahmed-D007A <ahmed.mamdouh8840@gmail.com>
2023-10-10 21:01:07 +0300

    debugging

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,8 +9,8 @@ def number_of_subscribers(subreddit):
     """
     Uses the reddit api to get the numbers of subscribers to a subreddit
     """
-    url = 'https://www.reddit.com/dev/api/' + subreddit + '/about/.json'
-    headers = {'User-Agent': "haha"}
+    url = 'https://reddit.com/r/' + subreddit + '/about/.json'
+    headers = {'User-Agent': "lala"}
     r = requests.get(url, headers=headers)
     try:
         return(r.json().get('data').get('subscribers'))
